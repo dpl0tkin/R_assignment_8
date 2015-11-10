dbf272a42e2f64d7dc5d83ebd36a49610c165426
David <dplotkin@ufl.edu>
2015-11-10 12:06:31 -0500

    Upload dataset to R and add column names to dataset.

--- R_assignment_8.R
+++ R_assignment_8.R
@@ -1,2 +1,7 @@
 #Problem 1
 
+mammal_sizes <- read.csv("data/MOMv3.3.txt", head = FALSE, sep = "\t", stringsAsFactors = FALSE, na.strings = "-999")
+
+colnames(mammal_sizes) <- c("continent", "status", "order", 
+                            "family", "genus", "species", "log_mass", "combined_mass", 
+                            "reference")
