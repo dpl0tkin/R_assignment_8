9823d392f5714b6881650849c23da99180fb3974
David <dplotkin@ufl.edu>
2015-11-13 12:37:23 -0500

    Calculated and printed average masses for extant and extinct mammals

--- R_assignment_8.R
+++ R_assignment_8.R
@@ -1,7 +1,27 @@
 #Problem 1
+library(dplyr)
 
 mammal_sizes <- read.csv("data/MOMv3.3.txt", head = FALSE, sep = "\t", stringsAsFactors = FALSE, na.strings = "-999")
 
 colnames(mammal_sizes) <- c("continent", "status", "order", 
                             "family", "genus", "species", "log_mass", "combined_mass", 
                             "reference")
+
+#Problem 2
+
+#Create separate tables for extant and extinct taxa
+extant_mamm <- filter(mammal_sizes, status == "extant")
+extinct_mamm <- filter(mammal_sizes, status == "extinct")
+#Remove null combined_mass values
+extant_mamm <- na.omit(extant_mamm)
+extinct_mamm <-na.omit(extinct_mamm)
+
+#calculate and print mean masses
+extant_mass = extant_mamm[["combined_mass"]]
+avg_extant_mass <- mean(extant_mass)
+
+extinct_mass = extinct_mamm[["combined_mass"]]
+avg_extinct_mass <- mean(extinct_mass)
+
+print(avg_extant_mass)
+print(avg_extinct_mass)
